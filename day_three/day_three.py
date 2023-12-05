ffeaa8f301d343e866d0ccf0ecbbd624d8ec7d33
Shisha <johns.blake921@gmail.com>
2023-12-04 21:27:52 -0600

    code golf changes

---
+++ day_three/day_three.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from itertools import permutations
+with open(Path("./d3data.txt"), encoding='utf8') as f: data = f.readlines()
+l = []
+[l.append(list(line.strip())) for line in data]
+row = 0
+total = 0
+for line in l:
+    f, num_string, col= False, "", 0
+    for char in line:
+        if char.isnumeric():
+            num_string+=char
+            col_back, col_front, row_back, row_front = col-1, col+1, row-1, row+1
+            perms = set(permutations([row_back, row, row_front, col_back, col, col_front ],2))
+            perms = [x for x in perms if x[0] >=0 and x[1] >= 0]
+            perms = [x for x in perms if col_back <= x[1] <= col_front and row_back <= x[0] <= row_front]
+            perms = [x for x in perms if x != (row, col)]
+            perms = [x for x in perms if x[0] < len(l) and x[1] < len(line)]
+            for i in perms:
+                if l[i[0]][i[1]] == ".":
+                    continue
+                elif l[i[0]][i[1]].isnumeric():
+                    continue
+                else:
+                    f = True
+            if col == len(line)-1 and f: total+=int(num_string); num_string=""; f=False
+        elif char == "." and not f: num_string=""
+        else: 
+            if f: total+=int(num_string); num_string=""; f=False
+        col += 1
+    row += 1
+print(total)
--- day_two/day_two.py
+++ day_two/day_two.py
@@ -1,85 +1,31 @@
 from pathlib import Path
-
-data = Path("./d2data.txt")
-
-
-def puzzle_one() -> None:
-    """The code solution for Advent of Code 2023 Day 2 Puzzle 1
-
-    Args:
-        None
-
-    Returns:
-        None
-    """
-    color_dict = {"red": 12, "green": 13, "blue": 14}
-    total = 0
-    with open(data) as f:
-        game_data = f.readlines()
-
-    for line in game_data:
-        colon_split = line.split(":")
-        game_id = colon_split[0]
-        break_flag = False
-        for pull in colon_split[1].split(";"):
-            color_cnt = pull.strip().split(",")
-            for color in color_cnt:
-                for key in color_dict:
-                    if key in color:
-                        temp_color = color.replace(key, "").strip()
-                        if int(temp_color) > color_dict[key]:
-                            break_flag = True
-        if not break_flag:
-            total += int(game_id.replace("Game ", ""))
-    print(total)
-
-
-def puzzle_two() -> None:
-    """The code solution for Advent of Code 2023 Day 2 Puzzle 2
-
-    Args:
-        None
-
-    Returns:
-        None
-    """
-    with open(data) as f:
-        game_data = f.readlines()
-    total = 0
-    for line in game_data:
-        red, blue, green = 0, 0, 0
-        pulls = line.split(":")[1].split(";")
-        for pull in pulls:
-            color_cnt = pull.strip().split(",")
-            for color in color_cnt:
-                if "red" in color:
-                    t_col = int(color.replace(" red", ""))
-                    if t_col > red:
-                        red = t_col
-                elif "blue" in color:
-                    t_col = int(color.replace(" blue", ""))
-                    if t_col > blue:
-                        blue = t_col
-                else:
-                    t_col = int(color.replace(" green", ""))
-                    if t_col > green:
-                        green = t_col
-        total += red * blue * green
-    print(total)
-
-
-def main() -> None:
-    """The main method
-
-    Args:
-        None
-
-    Returns:
-        None
-    """
-    puzzle_one()
-    puzzle_two()
-
-
-if __name__ == "__main__":
-    main()
+color_dict, total = {"red": 12, "green": 13, "blue": 14}, 0
+with open(Path("./d2data.txt")) as f: game_data = f.readlines()
+for line in game_data:
+    colon_split, game_id, break_flag = line.split(":"), line.split(":")[0], False
+    for pull in colon_split[1].split(";"):
+        color_cnt = pull.strip().split(",")
+        for color in color_cnt:
+            for key in color_dict:
+                if key in color:
+                    temp_color = color.replace(key, "").strip()
+                    if int(temp_color) > color_dict[key]: break_flag = True
+    if not break_flag:total += int(game_id.replace("Game ", ""))
+print(f'Puzzle One Total: {total}')
+total = 0
+for line in game_data:
+    red, blue, green, pulls = 0, 0, 0, line.split(":")[1].split(";")
+    for pull in pulls:
+        color_cnt = pull.strip().split(",")
+        for color in color_cnt:
+            if "red" in color:
+                t_col = int(color.replace(" red", ""))
+                if t_col > red: red = t_col
+            elif "blue" in color:
+                t_col = int(color.replace(" blue", ""))
+                if t_col > blue: blue = t_col
+            else:
+                t_col = int(color.replace(" green", ""))
+                if t_col > green: green = t_col
+    total += red * blue * green
+print(f'Puzzle Two total {total}')
